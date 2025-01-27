28c60eec8a6c6e1f5793bcd32eff28a3b09095c3
sreejith kr <srkr335@gmail.com>
2025-01-27 13:35:10 +0530

    ecommerce protal for HYZ

---
+++ database/seeds/SettingTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data=array(
+            'description'=>"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspiciatis unde sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspiciatis unde omnis iste natus error sit voluptatem Excepteu
+
+                            sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspiciatis Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspi deserunt mollit anim id est laborum. sed ut perspi.",
+            'short_des'=>"Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.",
+            'photo'=>"image.jpg",
+            'logo'=>'logo.jpg',
+            'address'=>"HYZ VENTURES INTL PRIVATE LIMITED  company in Kerala,Technopark Trivandrum, Padmanabham Building, Technopark Campus, Thiruvananthapuram, Kerala 695581",
+            'email'=>"hyzventures@gmail.com",
+            'phone'=>"+060 (200) 801-582",
+        );
+        DB::table('settings')->insert($data);
+    }
+}
